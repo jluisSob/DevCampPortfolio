72ce52acb7ef37bab83ad31a80edff911477d0e9
Jose Soberanes <jlcsob9@gmail.com>
2022-11-17 20:56:44 -0600

    Implemented concern for managing image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   def self.angular
@@ -8,11 +9,11 @@ class Portfolio < ApplicationRecord
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
 
-  afer_initialize :set_defaults
+  after_initialize :set_defaults
 
   def set_defaults
     # Let's have the ability to set the default values of the main_image and thumb_image
-    self.main_image ||= "http://placehold.it/600x400"
-    self.thumb_image ||= "http://placehold.it/350x200"
+    self.main_image ||= Placeholder.image_generator(height: 600, width: 400)
+    self.thumb_image ||= Placeholder.image_generator(height: 350, width: 200)
   end
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,11 @@
 class Skill < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :percent_utilized
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    # Let's have the ability to set the default values of the main_image and thumb_image
+    self.badge ||= Placeholder.image_generator(height: 250, width: 250)
+  end
 end
